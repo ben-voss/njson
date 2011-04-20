acf96bd6ff4c8da9ff87e4cb00fe15a246229e55
ben.james.voss@gmail.com <ben.james.voss@gmail.com@a42b7c2b-c954-b6f0-c883-39cf3a344041>
2011-04-20 13:30:51 +0000

    Added licence details

--- NJson/JsonParser.cs
+++ NJson/JsonParser.cs
@@ -1,3 +1,30 @@
+//
+// NJson - JSON Library for .Net.
+//    Copyright (C) 2011 Ben Voß
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Portions of this program are based on the following sources:
+// 
+// RFC-4627 as described at the following URL:
+//
+// 		http://www.ietf.org/rfc/rfc4627.txt?number=4627
+//
+// JSON.org
+//		http://www.json.org
+//		http://www.json.org/example.html
+//
 using System;
 using System.Text;
 using System.Globalization;
