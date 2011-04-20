b672b222979bf5147956feb0b34928ef9520e54c
ben.james.voss@gmail.com <ben.james.voss@gmail.com@a42b7c2b-c954-b6f0-c883-39cf3a344041>
2011-04-20 13:30:51 +0000

    Added licence details

--- trunk/NJson/AssemblyInfo.cs
+++ trunk/NJson/AssemblyInfo.cs
@@ -1,3 +1,30 @@
+//
+// NJson - JSON Library for .Net.
+//    Copyright (C) 2011 Ben Voß
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Portions of this program are based on the following sources:
+// 
+// RFC-4627 as described at the following URL:
+//
+// 		http://www.ietf.org/rfc/rfc4627.txt?number=4627
+//
+// JSON.org
+//		http://www.json.org
+//		http://www.json.org/example.html
+//
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -5,19 +32,19 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("NJson")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("JSON Library for .Net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Ben Voß")]
+[assembly: AssemblyProduct("NJson")]
+[assembly: AssemblyCopyright("2011 Ben Voß")]
+[assembly: AssemblyTrademark("NJson")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
