8a7f18338fd8068da49e46381465c16b447960b8
ben.james.voss@gmail.com <ben.james.voss@gmail.com@a42b7c2b-c954-b6f0-c883-39cf3a344041>
2011-04-21 08:45:20 +0000

    Rewrote the builder to use the writer

--- NJson/JsonBuilder.cs
+++ NJson/JsonBuilder.cs
@@ -27,115 +27,80 @@
 //
 using System;
 using System.IO;
+using System.Text;
 using System.Collections.Generic;
 
 namespace NJson
 {
 	public class JsonBuilder
 	{
-		public JsonBuilder ()
+		private JsonBuilder ()
 		{			
 		}
 		
-		public void Build(TextWriter writer, Object obj) {
+		public static void Build(StringBuilder builder, object obj) {
+			if (builder == null)
+				throw new ArgumentNullException("builder", "The value specified for the 'builder' argument must not be null.");
+			
+			Build(JsonWriter.Create(builder), obj);						
+		}
+		
+		public static void Build(TextWriter writer, object obj) {
+			if (writer == null)
+				throw new ArgumentNullException("writer", "The value specified for the 'writer' argument must not be null.");
+			
+			Build(JsonWriter.Create(writer), obj);			
+		}
+		
+		public static void Build(Stream stream, object obj) {
+			if (stream == null)
+				throw new ArgumentNullException("stream", "The value specified for the 'stream' argument must not be null.");
+			
+			Build(JsonWriter.Create(stream), obj);
+		}
+		
+		public static void Build(String file, object obj) {
+			if (file == null)
+				throw new ArgumentNullException("file", "The value specified for the 'file' argument must not be null.");
+			
+			Build(JsonWriter.Create(file), obj);			
+		}
+		
+		public static void Build(JsonWriter writer, Object obj) {
+			if (writer == null)
+				throw new ArgumentNullException("writer", "The value specified for the 'writer' argument must not be null.");
+			
 			if (obj == null)
-				Build(writer);
+				writer.WriteNullValue();
 			else if (obj is IEnumerable<Object>)
-				Build(writer, (IEnumerable<Object>)obj);
+			{
+				writer.WriteStartArray();
+
+				foreach (Object entry in (IEnumerable<Object>)obj)
+					Build(writer, entry);
+			
+				writer.WriteEnd();
+			}
 			else if (obj is IEnumerable<KeyValuePair<String, Object>>)
-				Build(writer, (IEnumerable<KeyValuePair<String, Object>>)obj);
+			{
+				writer.WriteStartObject();
+
+				foreach (KeyValuePair<String, Object> pair in (IEnumerable<KeyValuePair<String, Object>>)obj)
+				{
+					writer.WriteName(pair.Key);
+					Build(writer, pair.Value);
+				}
+			
+				writer.WriteEnd();
+			}
 			else if (obj is String)
-				Build(writer, (String)obj);
+				writer.WriteValue((String)obj);
 			else if (obj is Decimal)
-				Build(writer, (Decimal)obj);
+				writer.WriteValue((Decimal)obj);
 			else if (obj is bool)
-				Build(writer, (bool)obj);
+				writer.WriteValue((bool)obj);
 			else
 				throw new Exception("Unknown type");
 		}
-		
-		public void Build(TextWriter writer, IEnumerable<KeyValuePair<String, Object>> objectStructure) {
-			writer.Write('{');
-			
-			IEnumerator<KeyValuePair<String, Object>> enumerator = objectStructure.GetEnumerator();
-			if (enumerator.MoveNext())
-			{
-				KeyValuePair<String, Object> pair = enumerator.Current;
-				Build(writer, pair.Key);
-				writer.Write(':');
-				Build(writer, pair.Value);				
-			}
-			
-			while (enumerator.MoveNext()) {
-				KeyValuePair<String, Object> pair = enumerator.Current;
-				writer.Write(',');
-				Build(writer, pair.Key);
-				writer.Write(':');
-				Build(writer, pair.Value);
-			}
-			
-			writer.Write('}');			
-		}
-		
-		public void Build(TextWriter writer, IEnumerable<Object> arrayStructure) {
-			writer.Write('[');
-			
-			IEnumerator<Object> enumerator = arrayStructure.GetEnumerator();
-			if (enumerator.MoveNext())
-				Build(writer, enumerator.Current);
-							
-			while (enumerator.MoveNext()) {
-				writer.Write(',');
-				Build(writer, enumerator.Current);
-			}
-			
-			writer.Write(']');
-		}
-		
-		public void Build(TextWriter writer, String stringValue) {
-			writer.Write('"');
-			
-			for (int i = 0; i < stringValue.Length; i++)
-			{
-				char c = stringValue[i];
-				
-				if (c == '"')
-					writer.Write("\\\"");
-				else if (c == '\r')
-					writer.Write("\\r");
-				else if (c == '\n')
-					writer.Write("\\n");
-				else if (c == '\t')
-					writer.Write("\\t");
-				else if (c == '\b')
-					writer.Write("\\b");
-				else if (c == '\f')
-					writer.Write("\\f");
-				else if (c < 32 && c > 126)
-				{
-					writer.Write("\\u");
-					writer.Write(Convert.ToString((int)c, 16).PadLeft(4, '0'));
-				} else 
-					writer.Write(c);
-			}
-
-			writer.Write('"');
-		}
-		
-		public void Build(TextWriter writer, Decimal decimalValue) {
-			writer.Write(decimalValue.ToString());
-		}
-		
-		public void Build(TextWriter writer, bool boolValue) {
-			if (boolValue)
-				writer.Write("true");
-			else
-				writer.Write("false");
-		}
-		
-		public void Build(TextWriter writer) {
-			writer.Write("null");
-		}
 	}
 }
-
